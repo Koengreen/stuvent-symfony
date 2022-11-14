077133df3433f7797819cc10ff5d7c84b4db8405
Koengreen <302261150@student.rocmondriaan.nl>
2022-11-14 12:37:49 +0100

    register werkt alleen niet samen met de More info function

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,6 +9,11 @@ use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\User;
+use App\Form\RegistrationFormType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 
 class HomeController extends AbstractController
@@ -23,31 +28,20 @@ class HomeController extends AbstractController
     }
 
 
-    #[Route('/register')]
-    public function number(): Response
-    {
-        $number = random_int(0, 100);
-
-        return $this->render('home/register.html.twig', [
-            'number' => $number,
-        ]);
-    }
-
 
     #[Route('/{id}', name: 'more_info')]
     public function showInfo(ManagerRegistry $doctrine, int $id): Response
     {
         $evt = $doctrine->getRepository(Event::class)->find($id);
-
-        if (!$evt) {
-            throw $this->createNotFoundException(
-
-                'no information found for id ' . $id
+#
+       if (!$evt) {
+           throw $this->createNotFoundException(
+             'no information found for id ' . $id
             );
         }
         return $this->render('home/info.html.twig', [
             'evt' => $evt,
-        ]);
+       ]);
 
     }
 }
